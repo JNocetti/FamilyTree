b9649f7599fb150344349c67dd8ebd85c6532cae
Juan Nocetti <juan.anocetti@gmail.com>
2021-10-26 22:56:48 -0300

    attemps

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,24 +7,6 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            /*
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
-            
-            n1.AddChildren(n2);
-            n1.AddChildren(n3);
-
-            n2.AddChildren(n4);
-            n2.AddChildren(n5);
-
-            n3.AddChildren(n6);
-            n3.AddChildren(n7);
-            */
             Person abuelo_madre = new Person ("Juan Carlos", 70);
             Node node_abuelo_madre = new Node (abuelo_madre);
             Person tío_madre = new Person ("Weston", 42);
